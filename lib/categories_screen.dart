c40655b89d3197dc0ed7208af18a8367e3a0a41e
Ruben <lukesruben@gmail.com>
2019-09-24 14:41:20 +0200

    added font and style

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,7 +6,11 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("MyMeals"),
+      ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(catData.title, catData.color))
             .toList(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
 
-void main(){
+void main() {
   runApp(MyMealsApp());
 }
 
@@ -11,7 +11,19 @@ class MyMealsApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyMeals',
       theme: ThemeData(
-        primaryColor: Colors.blue
+          fontFamily: 'Railway',
+          primaryColor: Colors.blue,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51,51,1)),
+              body2: TextStyle(color: Color.fromRGBO(20,51,51,1)),
+            title: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold
+            )
+          ),
       ),
       home: CategoriesScreen(),
     );
